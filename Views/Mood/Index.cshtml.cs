999d5ce2e135b79a90532fc95bd697183a455f3f
sanches3020 <evmenovasofia7@gmail.com>
2025-06-21 11:33:40 +0300

    Views

---
+++ Views/Mood/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Mood
+{
+    public class CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Notes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Notes
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
