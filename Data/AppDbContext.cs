b4afb7dce22ac613a7978e2d299f6a982350dacc
sanches3020 <evmenovasofia7@gmail.com>
2025-06-21 11:27:26 +0300

    Data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodMapper.Models;
+
+namespace MoodMapper.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Mood> Moods { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        public DbSet<EmotionEntry> EmotionEntries { get; set; }
+        public DbSet<Settings> Settings { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
