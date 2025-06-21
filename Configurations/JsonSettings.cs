1e05b0d770c773bd2c75352feb719db9a0a12d18
sanches3020 <evmenovasofia7@gmail.com>
2025-06-21 11:27:48 +0300

    Configuration

---
+++ Configurations/JsonSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace MoodMapper.Configurations
+{
+    /// <summary>
+    /// Класс настроек для экспорта и импорта данных в формате JSON.
+    /// Значения этих настроек будут загружаться из секции "JsonSettings" файла appsettings.json.
+    /// </summary>
+    public class JsonSettings
+    {
+        /// <summary>
+        /// Путь к файлу, в который будут экспортироваться данные.
+        /// </summary>
+        public required string ExportFilePath { get; set; }
+
+        /// <summary>
+        /// Путь к файлу, из которого будут импортироваться данные.
+        /// </summary>
+        public required string ImportFilePath { get; set; }
+    }
+}
